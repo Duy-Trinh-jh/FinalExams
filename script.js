6b4c13998ab6aef624da155178fb17a09df00116
Duy-Trinh-jh <duy.trinh@journeyh.io>
2022-04-01 11:46:17 +0700

    Add question data

---
+++ script.js
@@ -0,0 +1,45 @@
+const choiceQuestionData = [
+  {
+    'question': 'If a = 1 and b = 2 then what is a * b?',
+    'answer' : [1, 2, 3, 4],
+    'correctAnswer': 2,
+  },
+  {
+    'question': 'If a = 2 and b = 2 then what is a + b?',
+    'answer' : [1, 2, 3, 4],
+    'correctAnswer': 4,
+  },
+  {
+    'question': 'If a = 4 and b = 1 then what is a - b?',
+    'answer' : [1, 2, 3, 4],
+    'correctAnswer': 3,
+  },
+  {
+    'question': 'If a = 2 and b = 2 then what is a / b?',
+    'answer' : [1, 2, 3, 4],
+    'correctAnswer': 1,
+  },
+  {
+    'question': 'If a = 2 and b = 2 then what is 2 * a + b?',
+    'answer' : [2, 4, 6, 8],
+    'correctAnswer': 6,
+  },
+]
+
+const eassyQuestionData = [
+  {
+    'question': 'How is the weather today?',
+  },
+  {
+    'question': 'What is your name?',
+  },
+  {
+    'question': 'Where are you from?',
+  },
+  {
+    'question': 'What is your favorite food?',
+  },
+  {
+    'question': 'What are your hobbies?',
+  },
+]
